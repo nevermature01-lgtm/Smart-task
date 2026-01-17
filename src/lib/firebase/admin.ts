4d8fc838fba47e81a48d9d0df20fa15ef2c4f217
nevermature <nevermature01@gmail.com>
2026-01-17 10:55:32 +0000

    do it

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,4 +1,5 @@
 import admin from 'firebase-admin';
+import { firebaseConfig } from '@/firebase/config';
 
 // This prevents re-initialization in a serverless environment.
 if (!admin.apps.length) {
@@ -7,6 +8,7 @@ if (!admin.apps.length) {
       // The credentials are automatically sourced from the GOOGLE_APPLICATION_CREDENTIALS
       // environment variable in the hosting environment (like Cloud Run, App Engine).
       // For local development, you need to set this environment variable to point to your service account key file.
+      projectId: firebaseConfig.projectId,
     });
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
